74ceff264e889acfcd4206510e0046ccff9f45af
ozzie <nextlife_ozzie@outlook.com>
2017-03-10 21:30:07 +0800

    v1

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 ﻿var ozApp = angular.module('ozApp', ['ngRoute']);
 
-ozApp.config(['$routeProvider', function ($routeProvider) {
+ozApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
     $routeProvider.when('/article', {
-        templateUrl:'partials/articles.html'
+        templateUrl: 'templateUrl/articles.html'
     }).otherwise({
         redirectTo: '/article'
+    });
+
+    // use the HTML5 History API
+    $locationProvider.html5Mode({
+        enabled: true
     });
 }]);
