7b041398096bee0dfdfee7eb73258dc83edb6448
ozzie <nextlife_ozzie@outlook.com>
2017-03-10 21:13:04 +0800

    Added Controller, app, partials

---
+++ js/app.js
@@ -0,0 +1,9 @@
+﻿var ozApp = angular.module('ozApp', ['ngRoute']);
+
+ozApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/article', {
+        templateUrl:'partials/articles.html'
+    }).otherwise({
+        redirectTo: '/article'
+    });
+}]);
