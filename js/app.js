d4b74294f298d084aae6c064210586fc62b9d095
ozzie <nextlife_ozzie@outlook.com>
2017-03-11 17:04:22 +0800

    sadfsf

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 ﻿var ozApp = angular.module('ozApp', ['ngRoute']);
 
-ozApp.config(['$routeProvider',  function ($routeProvider) {
+ozApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
     $routeProvider.when('/home', {
-        templateUrl: 'partials/home.html'
+        templateUrl: '/partials/home.html'
     }).when('/articles', {
-        templateUrl: 'partials/articles.html'
+        templateUrl: '/partials/articles.html'
     }).when('/programing', {
-        templateUrl: 'partials/programing.html'
+        templateUrl: '/partials/programing.html'
     }).when('/tips', {
-        templateUrl: 'partials/tips.html',
+        templateUrl: '/partials/tips.html',
         controller: 'TipsCtrl'
     }).otherwise({
         redirectTo: '/home'
     });
 
     // use the HTML5 History API
-    //$locationProvider.html5Mode({
-    //    enabled: true
-    //});
+    $locationProvider.html5Mode({
+        enabled: true
+    });
 }]);
