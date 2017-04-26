21824830d5d32f0528a061a1c37d4d0d8ee1afa0
ozzieKang <p-ozzie-q@hotmail.com>
2017-04-26 17:32:09 +0800

    201704261732

---
+++ js/service.js
@@ -0,0 +1,39 @@
+﻿NGAApp.service('authService', ['$http', function ($http) {
+    var getUser = function () {
+        var link = "../AI/data/UserControl.aspx";
+        return $http.get(link).then(function (response) {
+            user = response.data.records;
+            return user;
+        });
+    };
+    return { getUser: getUser };
+}]);
+
+NGAApp.directive('restrict', function (authService) {
+    return {
+        restrict: 'A',
+        priority: 10,
+        scope: false,
+        link: function () { },
+        compile: function (element, attr, linker) {
+
+            var user = authService.getUser();
+            user.then(function (result) {
+                var accessDenied = true;
+                var attributes = attr.access.split(" ");
+                for (var i in attributes) {
+                    if (result[0].Role == attributes[i]) {
+                        accessDenied = false;
+                    }
+                }
+
+                if (accessDenied) {
+                    element.children().remove();
+                    element.remove();
+                }
+            })
+
+        }
+    };
+
+});
